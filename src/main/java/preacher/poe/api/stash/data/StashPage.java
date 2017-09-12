7facacf0ba38bfc9b779878ac85e06f9732dcb04
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2017-09-11 21:54:49 -0400

    Removed useless package path artifact created during refractoring

--- src/main/java/preacher/poe/api/stash/data/StashPage.java
+++ src/main/java/preacher/poe/api/stash/data/StashPage.java
@@ -3,5 +3,5 @@ package preacher.poe.api.stash.data;
 public class StashPage {
     public String next_change_id;
     
-    public preacher.poe.api.stash.data.Stash[] stashes;
+    public Stash[] stashes;
 }
